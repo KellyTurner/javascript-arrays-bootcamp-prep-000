0199801e9e10d6112190e695bb1d6f2d3378c824
Kelly turner <klynnturner@gmail.com>
2017-12-01 22:01:42 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,7 +1,7 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 
 function addElementToBeginningOfArray(array, element) {
-  var array = [...array, "element"]
+  var array = [...array, (0)]
   return array
 }
 function addElementToEndOfArray (array,element) {
